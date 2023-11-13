62531f6d6f7abfbd68f9b4b6a2e0d4413f25b0ab
Kristin Levine <kristinjlevine@gmail.com>
2023-11-13 14:20:33 -0600

    updated unit tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,22 @@
 public class RewardValue {
     private double cashValue;
-    private double milesValue;
+    private int milesValue;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
+        this.milesValue = (int) (cashValue / 0.0035);
     }
 
-    public RewardValue(double milesValue, boolean isMiles) {
-        if (isMiles) {
+    public RewardValue(int milesValue) {
             this.milesValue = milesValue;
             this.cashValue = milesValue * 0.0035;
-        }
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
+    public int getMilesValue() {
         return milesValue;
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        double convertedMilesValue = (int) (cashValue / 0.0035);
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(convertedMilesValue, rewardValue.getMilesValue());
     }
-
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        double convertedCashValue = milesValue * 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(convertedCashValue, rewardValue.getCashValue());
     }
+
 }
